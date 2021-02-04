9efe977f0280170a7b8eef1aa3de4d9c2ebe519d
hung.han <redkyo017@gmai.com>
2021-02-04 13:51:17 +0700

    update check instance & env param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,18 +14,23 @@ export class AppController {
     // const results = Object.create(null); // Or just '{}', an empty object
 
     for (const name of Object.keys(nets)) {
-        if (name === "en0") {
-          return nets[name].address
+      if (name === 'en0') {
+        for (const net of nets[name]) {
+          // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+          if (net.family === 'IPv4' && !net.internal) {
+            return `${net.address}-${process.env.TEST_PARAM}`;
+          }
         }
-        // for (const net of nets[name]) {
-        //     // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        //     if (net.family === 'IPv4' && !net.internal) {
-        //         if (!results[name]) {
-        //             results[name] = [];
-        //         }
-        //         results[name].push(net.address);
-        //     }
-        // }
+      }
+      // for (const net of nets[name]) {
+      //     // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      //     if (net.family === 'IPv4' && !net.internal) {
+      //         if (!results[name]) {
+      //             results[name] = [];
+      //         }
+      //         results[name].push(net.address);
+      //     }
+      // }
     }
     return this.appService.getHello();
   }

f0b64f8a5bd3d8c2267e37151c8ccecb84bfeb18
hung.han <redkyo017@gmai.com>
2021-02-04 10:45:52 +0700

    check container instance

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,25 @@ export class AppController {
   @Get()
   @HttpCode(200)
   getHello(): string {
+    const { networkInterfaces } = require('os');
+
+    const nets = networkInterfaces();
+    // const results = Object.create(null); // Or just '{}', an empty object
+
+    for (const name of Object.keys(nets)) {
+        if (name === "en0") {
+          return nets[name].address
+        }
+        // for (const net of nets[name]) {
+        //     // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        //     if (net.family === 'IPv4' && !net.internal) {
+        //         if (!results[name]) {
+        //             results[name] = [];
+        //         }
+        //         results[name].push(net.address);
+        //     }
+        // }
+    }
     return this.appService.getHello();
   }
 }
